4188b531c6d866c3f101035e0f09252ead9d1459
rnktlhr <rinakitluhur10@gmail.com>
2025-11-27 13:18:07 +0700

    menambahkan interface pada containerapp

---
+++ app/src/main/java/com/example/praktikum7/repositori/ContainerApp.kt
@@ -0,0 +1,9 @@
+package com.example.praktikum7.repositori
+
+import android.content.Context
+import com.example.praktikum7.room.DatabaseSiswa
+
+interface ContainerApp {
+    val repositoriSiswa: RepositoriSiswa
+}
+
