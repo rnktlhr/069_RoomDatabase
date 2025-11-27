fab581dba6e9723a4197937721200aff52217fa7
rnktlhr <rinakitluhur10@gmail.com>
2025-11-27 13:04:43 +0700

    menambahkan abstrak class

--- app/src/main/java/com/example/praktikum7/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/praktikum7/room/DatabaseSiswa.kt
@@ -1,5 +1,12 @@
 package com.example.praktikum7.room
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 
 @Database(entities = [Siswa::class], version = 1, exportSchema = false)
+
+abstract class DatabaseSiswa : RoomDatabase(){
+    abstract fun SiswaDao(): SiswaDao
+
+
+}
