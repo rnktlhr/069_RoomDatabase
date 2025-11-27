43be7cac952fdcb467f1f76bb5c49c4e81535fb4
rnktlhr <rinakitluhur10@gmail.com>
2025-11-27 14:05:07 +0700

    membuat penyedia view model

---
+++ app/src/main/java/com/example/praktikum7/viewmodel/provider/PenyediaViewModel.kt
@@ -0,0 +1,24 @@
+package com.example.praktikum7.viewmodel.provider
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.praktikum7.repositori.AplikasiSiswa
+import com.example.praktikum7.viewmodel.EntryViewModel
+import com.example.praktikum7.viewmodel.HomeViewModel
+
+
+object PenyediaViewModel{
+    val Factory = viewModelFactory{
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+        initializer {
+            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+    }
+}
+
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
