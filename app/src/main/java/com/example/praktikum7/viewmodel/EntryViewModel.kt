88ab20aa3f632a0a284c418af06e9b21f6ec8c97
rnktlhr <rinakitluhur10@gmail.com>
2025-11-27 13:52:52 +0700

    menambahkan private fun

---
+++ app/src/main/java/com/example/praktikum7/viewmodel/EntryViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.praktikum7.viewmodel
+
+
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.praktikum7.repositori.RepositoriSiswa
+import com.example.praktikum7.room.Siswa
+
+class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean{
+        return with(uiState){
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
