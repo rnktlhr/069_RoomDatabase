45be8fc7f13089399d32e06299222a9c9e7d13cf
rnktlhr <rinakitluhur10@gmail.com>
2025-11-27 13:54:31 +0700

    menambahkan  data class detailsiswa

--- app/src/main/java/com/example/praktikum7/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/praktikum7/viewmodel/EntryViewModel.kt
@@ -38,3 +38,16 @@ data class UIStateSiswa(
     val isEntryValid: Boolean = false
 )
 
+data class DetailSiswa(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = "",
+)
+
+/**
+ * Fungsi untuk mengkonversi data input ke data dalam tabel sesuai jenis datanya
+ */
+
+
+
