c6270f2115f19e551a2945dcc86575dac4123cc7
rnktlhr <rinakitluhur10@gmail.com>
2025-11-27 13:32:58 +0700

    menambahkan obhek pad destinasi home

---
+++ app/src/main/java/com/example/praktikum7/view/route/DestinasiEntry.kt
@@ -0,0 +1,8 @@
+package com.example.praktikum7.view.route
+
+import com.example.praktikum7.R
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
