4d62b46377c6382d88e4a430695753cf2de824ca
rnktlhr <rinakitluhur10@gmail.com>
2025-11-27 13:00:28 +0700

    menambahkan fun getallsiswa pada dao

--- app/src/main/java/com/example/praktikum7/room/SiswaDao.kt
+++ app/src/main/java/com/example/praktikum7/room/SiswaDao.kt
@@ -2,8 +2,10 @@ package com.example.praktikum7.room
 
 import androidx.room.Dao
 import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface SiswaDao {
     @Query("SELECT * from tblsiswa ORDER BY nama ASC")
+    fun getAllSiswa(): Flow<List<Siswa>>
 }
