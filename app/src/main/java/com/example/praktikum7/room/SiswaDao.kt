616f940b62b747df3a97f18f0e44134e897514e3
rnktlhr <rinakitluhur10@gmail.com>
2025-12-04 11:30:40 +0700

    menambah query dan deletae di siswa dao

--- app/src/main/java/com/example/praktikum7/room/SiswaDao.kt
+++ app/src/main/java/com/example/praktikum7/room/SiswaDao.kt
@@ -1,6 +1,7 @@
 package com.example.praktikum7.room
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
@@ -13,4 +14,10 @@ interface SiswaDao {
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(siswa: Siswa)
+
+    @Query("SELECT * from tblSiswa WHERE id = :id")
+    fun getSiswa(id: Int): Flow<Siswa>
+
+    @Delete
+    suspend fun delete(siswa: Siswa)
 }
