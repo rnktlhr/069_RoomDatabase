fb075d4fd19451f1ab77cd872c589ce874d749b3
rnktlhr <rinakitluhur10@gmail.com>
2025-12-04 11:42:35 +0700

    update repositorysiswa

--- app/src/main/java/com/example/praktikum7/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/praktikum7/repositori/RepositoriSiswa.kt
@@ -7,6 +7,8 @@ import kotlinx.coroutines.flow.Flow
 interface RepositoriSiswa {
     fun getAllSiswaStream(): Flow<List<Siswa>>
     suspend fun insertSiswa(siswa: Siswa)
+    fun getSiswaStream(id: Int): Flow<Siswa?>
+    suspend fun deleteSiswa(siswa: Siswa)
 }
 
 class OfflineRepositoriSiswa(
@@ -16,4 +18,6 @@ class OfflineRepositoriSiswa(
         .getAllSiswa()
     override suspend fun insertSiswa(siswa: Siswa) = siswaDao
         .insert(siswa)
+    override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
+    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
 }
